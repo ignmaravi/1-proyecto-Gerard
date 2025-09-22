f0f5e9581dc217e4a88f99451ee9fe4b7abbbc44
ferran <ferbeltransan@gmail.com>
2025-09-22 19:06:24 +0200

    Prueba JS añadirNotas

--- indexJS.js
+++ indexJS.js
@@ -1,1 +1,11 @@
-alert("Hola Mundo");
+const añadirNota = document.getElementById('addNoteBtn');
+const contenedorNotas = document.getElementById('notesContainer');
+
+añadirNota.addEventListener('click', () => {
+    const nuevaNota = prompt('Escribe tu nota:');
+
+    if(nuevaNota === "" || nuevaNota === null) {
+        alert("Escribe algo!");
+    } 
+    
+});
